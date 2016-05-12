0aab537a9ce28b3f4eba00372d230aea2ac7707b
Subhash Fizzy <subhashchandra@fizzysoftware.com>
2016-05-12 17:34:57 +0530

    added js for chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,15 @@
 class MessagesController < ApplicationController
 
-  def create
+def create
     message = Message.new(message_params)
     message.user = current_user
     if message.save
-      # do some stuff
-    else 
-      redirect_to chatrooms_path
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.username
+      head :ok
     end
   end
-
   private
 
     def message_params
